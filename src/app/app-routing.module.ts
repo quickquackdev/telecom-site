33015cf2be6f44a2c1f87573e774212616a1e982
quickquackdev <eperdomo@skillstorm.com>
2021-10-28 15:26:11 -0400

    added devices component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DevicesComponent } from './devices/devices.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // redirect home to dashboard
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'myDevices', component: DevicesComponent }
+  // paths to be added when components are in
+  //{ path: 'User', component: UserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DevicesComponent } from './devices/devices.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    DevicesComponent
   ],
   imports: [
     BrowserModule,
