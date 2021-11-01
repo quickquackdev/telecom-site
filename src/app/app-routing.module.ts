f0958d1add869138f248a91d264b4c68885891ff
quickquackdev <eperdomo@skillstorm.com>
2021-11-01 14:14:38 -0400

    added a route in the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DeviceDetailComponent } from './device-detail/device-detail.component';
 import { DevicesComponent } from './devices/devices.component';
 
 const routes: Routes = [
   // redirect home to dashboard
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'myDevices', component: DevicesComponent }
+  { path: 'myDevices', component: DevicesComponent },
+  { path: 'detail/:id', component: DeviceDetailComponent }
   // paths to be added when components are in
   //{ path: 'User', component: UserComponent}
 ];
